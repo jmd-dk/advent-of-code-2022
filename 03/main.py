a072249c67dabcff6b65a79299eebf0b141eb7dc
Jeppe Dakin <jeppe_dakin@hotmail.com>
2022-12-03 14:28:38 +0100

    solve day 3

---
+++ 03/main.py
@@ -0,0 +1,27 @@
+# Read data
+def compartmentalize(s):
+    s = s.strip()
+    return s[:len(s)//2], s[len(s)//2:]
+with open('input.txt', 'r') as f:
+    rucksacks = list(map(compartmentalize, f))
+
+# Part one
+def get_common_item(*containers):
+    items = set(containers[0])
+    for container in containers[1:]:
+        items &= set(container)
+    return items.pop()
+def get_priority(item):
+    return 1 + ord(item.lower()) - ord('a') + 26*item.isupper()
+priority_sum = 0
+for rucksack in rucksacks:
+    priority_sum += get_priority(get_common_item(*rucksack))
+print('part one:', priority_sum)
+
+# Part two
+priority_sum = 0
+groupsize = 3
+for group in zip(*(rucksacks[i::groupsize] for i in range(groupsize))):
+    priority_sum += get_priority(get_common_item(*(''.join(elf) for elf in group)))
+print('part two:', priority_sum)
+
